de716e61d72843b91f8d3a7d5851318c940b2a0f
agurk <timothy.moll+github@gmail.com>
2014-12-30 22:47:38 +0000

    Working towards becoming a generic Processor for generated raw strings

--- Processors/Processor_Aqua.pm
+++ Processors/Processor_Aqua.pm
@@ -26,11 +26,17 @@ use constant DATE_INDEX => 0;
 use constant DESCRIPTION_INDEX => 2;
 use constant AMOUNT_INDEX => 3;
 use constant CREDIT_DEBIT_INDEX => 4;
- 
+use constant FX_AMOUNT_INDEX => 5;
+use constant FX_CCY_INDEX => 6;
+use constant FX_RATE_INDEX => 7;
+use constant COMMISSION_INDEX => 8;
+
+# Generated CSV line format is:
+# transaction date; processed date; description; amount; debit/credit; fx amount; fx ccy; fx rate; commission
 sub processRawLine
 {
 	my ($self, $line, $rid, $aid, $ccy) = @_;
-    my @lineParts=$self->_splitLine($line);
+    my @lineParts=split(/;/, $line);
 	$lineParts[AMOUNT_INDEX] *= -1 if ($lineParts[CREDIT_DEBIT_INDEX] =~ m/DR/);
     my $expense = Expense->new (
 							AccountID => $aid,
@@ -40,5 +46,16 @@ sub processRawLine
 						    Currency => $ccy,
                         );
 	$expense->addRawID($rid);
+	$self->_addFX($expense, \@lineParts);
 	return $expense;
 }
+
+sub _addFX
+{
+	my ($self, $expense, $lineParts);
+	$expense->setFXAmount($1) if (defined $$lineParts[FX_AMOUNT_INDEX]);
+	$expense->setFXCCY($2) if (defined $$lineParts[FX_CCY_INDEX]);
+	$expense->setFXRate($3) if (defined $$lineParts[FX_RATE_INDEX]);
+	$expense->setCommission($4) if (defined $$lineParts[COMMISSION_INDEX]);
+}
+
