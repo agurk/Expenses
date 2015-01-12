2d29c2e8431576a3ead17fbe5a47b0a514dc3ead
Agurk <timothy.moll+github@gmail.com>
2015-01-12 21:32:20 +0200

    Added a link to load more expenses data from a jquery call

---
+++ website/ItemView.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sqlite3
+
+class ItemView:
+
+    def __init__(self, expenseID):
+        self.expenseID = expenseID
+
+    def RawStr(self):
+        conn = sqlite3.connect('../expenses.db')
+        conn.text_factory = str 
+        query = 'select rawstr from rawdata, expenserawmapping where rawdata.rid=expenserawmapping.rid and expenserawmapping.eid = {0};'.format(self.expenseID)
+        cursor = conn.execute(query)
+        for row in cursor:
+            totalAmount = row[0]
+        return totalAmount
