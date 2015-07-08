570dc7a5694d1eb0826412b5836c3bfd6ee39807
Agurk <timothy.moll+github@gmail.com>
2015-07-08 22:17:58 +0300

    First version of a class to display different CCYs. Currently just a test version of the class with static fx rates

---
+++ website/FXValues.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import sqlite3
+import time
+import datetime
+from datetime import date, timedelta
+import config
+import expensesSQL
+
+class FXValues:
+
+    def __init__(self):
+        self.foo='true'
+        #conn = sqlite3.connect(config.SQLITE_DB)
+#=        conn.text_factory = str 
+##        cursor = conn.execute(expensesSQL.getCCYFormats())
+#        for row in cursor:
+#            self.ccyFormats[row[0]] = row[1]
+
+    def FXAmount(self, amount, baseCCY, ccy, date):
+        if baseCCY== ccy:
+            return amount
+        if ccy=='EUR':
+            return self._toEur(amount, baseCCY)
+        if ccy=='GBP':
+            return self._toGbp(amount, baseCCY)
+        if ccy == 'DKK':
+            return self._toDkk(amount, baseCCY)
+
+    def _toEur(self, amount, baseCCY):
+        if baseCCY == 'GBP':
+            return amount / 0.72
+        if baseCCY == 'DKK':
+            return amount / 7.46
+
+    def _toGbp(self, amount, baseCCY):
+        if baseCCY == 'EUR':
+            return amount / 1.39
+        if baseCCY == 'DKK':
+            return amount / 10.36
+
+    def _toDkk(self, amount, baseCCY):
+        if baseCCY == 'GBP':
+            return amount / 0.097
+        if baseCCY == 'EUR':
+            return amount / 0.13
+
