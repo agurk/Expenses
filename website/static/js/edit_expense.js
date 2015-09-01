0fdd712b387533d4e7e41b8d6c7ab3a704b96c8a
agurk <timothy.moll+github@gmail.com>
2015-09-01 23:54:31 +0300

    Added cursor shortcut to date box. Save now returns you to where you came from

--- website/static/js/edit_expense.js
+++ website/static/js/edit_expense.js
@@ -27,6 +27,24 @@ function save_expense(eid) {
 			+'&fxRate='+		$('#exFXRate').val()
 			+'&commission='+	$('#exCommission').val()
 	);
+	window.history.back();
+}
 
+function cursor_date(e) {
+	e = e || window.event;
+	switch (e.keyCode) {
+		case 38:
+			change_date(1);
+			break;
+		case 40:
+			change_date(-1);
+			break;
+	}
+}
+
+function change_date(delta) {
+	var d = new Date(document.getElementById('exDate').value);
+	d.setDate(d.getDate() + delta);
+	document.getElementById('exDate').value = (d.toISOString().slice(0,10));
 }
 
