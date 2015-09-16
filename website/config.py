7b1e9459cd01b40855336c688e1f4bf6978cc04d
agurk <timothy.moll+github@gmail.com>
2015-09-16 23:17:43 +0300

    Config needed for image handler to work

--- website/config.py
+++ website/config.py
@@ -2,4 +2,15 @@
 
 import warnings
 
-SQLITE_DB='/home/timothy/bin/Expenses/expenses.db'
+SQLITE_DB='/home/timothy/src/Expenses/expenses.db'
+
+
+## Image Config ##
+IMAGE_DIR='/home/timothy/src/Expenses/data/documents'
+
+THUMBNAIL_CACHE='/home/timothy/src/Expenses/data/documents/thumbs'
+THUMBNAIL_SIZE=128, 128
+THUMBNAIL_FORMAT='JPEG'
+
+IMAGE_404='IMG_0001.JPG'
+
