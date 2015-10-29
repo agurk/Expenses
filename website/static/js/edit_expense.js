4701d3d1d8893855768b755e4249ff6be254abdb
agurk <timothy.moll+github@gmail.com>
2015-10-29 21:17:12 +0200

    Updating expense page to use new date control

--- website/static/js/edit_expense.js
+++ website/static/js/edit_expense.js
@@ -56,22 +56,3 @@ function verify_expense_data() {
 		return false;
 	}
 }
-
-function cursor_date(e) {
-	e = e || window.event;
-	switch (e.keyCode) {
-		case 38:
-			change_date(1);
-			break;
-		case 40:
-			change_date(-1);
-			break;
-	}
-}
-
-function change_date(delta) {
-	var d = new Date(document.getElementById('exDate').value);
-	d.setDate(d.getDate() + delta);
-	document.getElementById('exDate').value = (d.toISOString().slice(0,10));
-}
-
