ae4809b8d3d3fa61c1a79d9d101c2bba31d0ae3f
agurk <timothy.moll+github@gmail.com>
2018-10-31 17:23:49 +0100

    Updates Aqua loader for changes to JSON payload

--- backend/ExpenseData/Processors/Processor_Aqua.pm
+++ backend/ExpenseData/Processors/Processor_Aqua.pm
@@ -32,7 +32,41 @@ sub processRawLine
 	my $data = decode_json $json;
 
 	use Data::Dumper;
-	#print Dumper $data;
+	print Dumper $data;
+
+	my $amount = $data->{'amount'} * -1;
+	my $date = $self->_formatDate2($data->{'effectiveDate'});
+
+    my $description = $data->{'description'};
+    $description =~ s/  */ /g;
+	my $expense = $self->_findExpense($aid, $data->{'referenceNbr'}, $date, $description, $amount, $ccy);
+	
+	unless (defined $expense)
+	{
+		$expense = Expense->new (
+										AccountID => $aid,
+										Date => $date,
+										Description => $description,
+										Amount => $amount,
+										Currency => $ccy,
+								);
+	}
+
+	$self->_addFX($expense, $data);
+    $self->_setTemporary($expense, $data);
+	$expense->addRawID($rid);
+#	# for temporary expenses to be updated to the right amount
+	$expense->setAmount($amount);
+	return $expense;
+}
+
+sub processRawLineOld
+{
+	my ($self, $json, $rid, $aid, $ccy) = @_;
+	my $data = decode_json $json;
+
+	use Data::Dumper;
+	print Dumper $data;
 
 	my $amount = $data->{'amount'} * -1;
 	my $date = $self->_formatDate($data->{'effectiveDate'});
@@ -66,6 +100,11 @@ sub _setTemporary
 	    $expense->setTemporary(0);
         $expense->setReference($data->{'tranRefNo'});
     }
+    if ($data->{'referenceNbr'})
+    {
+	    $expense->setTemporary(0);
+        $expense->setReference($data->{'referenceNbr'});
+    }
     else
     {
 	    $expense->setTemporary(1);
@@ -89,12 +128,20 @@ sub _formatDate
 	return $3.'-'.$2.'-'.$1
 }
 
+sub _formatDate2
+{
+	my ($self, $date) = @_;
+	$date =~ m/([0-9]{4}-[0-9]{2}-[0-9]{2})/;
+	return $1;
+}
+
 sub _addFX
 {
 	my ($self, $expense, $data) = @_;
 	$expense->setFXAmount($data->{'foreignTxnAmnt'}) if defined($data->{'foreignTxnAmnt'});
 	$expense->setFXCCY($data->{'foreignTxnCurrency'}) if defined ($data->{'foreignTxnCurrency'});
 	$expense->setFXRate($data->{'foreignExchangeRate'}) if defined ($data->{'foreignExchangeRate'});
+	$expense->setFXRate($data->{'exchangeRate'}) if ((defined ($data->{'exchangeRate'})) && $data->{'exchangeRate'} ne '0');
 	#$expense->setCommission($data->{''}) if defined ($data->{''});
 }
 
@@ -110,7 +157,7 @@ sub _chooseSimilarExpense
     {
         next if ( $amount * $$row[1] < 0 );
         my $diff = abs(abs($$row[1]) - abs($amount)) / abs($amount);
-        next unless ($diff <= 0.05);
+        next unless ($diff <= 0.02);
         $$row[2] =~ s/ //g;
         $$row[2] = uc($$row[2]);
         next unless ($description eq $$row[2]);
