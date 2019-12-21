0a7a608e8d0451df011ffe460b2a12e04d4e35c9
agurk <timothy.moll+github@gmail.com>
2019-12-21 22:13:17 +0100

    Fixes error in SQL statement so results are now correct

--- b2/src/b2/docexmappings/db.go
+++ b2/src/b2/docexmappings/db.go
@@ -1,12 +1,12 @@
 package docexmappings
 
 import (
-    "database/sql"
-    "errors"
+	"database/sql"
+	"errors"
 )
 
 func loadMapping(dmid uint64, db *sql.DB) (*Mapping, error) {
-    rows, err := db.Query(`
+	rows, err := db.Query(`
         select
             did,
             eid,
@@ -15,53 +15,51 @@ func loadMapping(dmid uint64, db *sql.DB) (*Mapping, error) {
             DocumentExpenseMapping
         where
             dmid = $1`,
-            dmid)
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
-    mapping := new(Mapping)
-    if rows.Next() {
-        err = rows.Scan(&mapping.DID,
-                        &mapping.EID,
-                        &mapping.Confirmed)
-        mapping.ID = dmid
-    } else {
-        return nil, errors.New("404")
-    }
-    if err != nil {
-        return nil, err
-    }
-    return mapping, nil
+		dmid)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	mapping := new(Mapping)
+	if rows.Next() {
+		err = rows.Scan(&mapping.DID,
+			&mapping.EID,
+			&mapping.Confirmed)
+		mapping.ID = dmid
+	} else {
+		return nil, errors.New("404")
+	}
+	if err != nil {
+		return nil, err
+	}
+	return mapping, nil
 }
-
 
 func findMappings(query *Query, db *sql.DB) ([]uint64, error) {
-    var sqlQuery string
-    var id uint64
-    if query.ExpenseId > 0 {
-        sqlQuery = "select mid from ExpenseRawMapping where eid = $1"
-        id = query.ExpenseId
-    } else if query.DocumentId > 0 {
-        sqlQuery = "select mid from ExpenseRawMapping where did = $1"
-        id = query.DocumentId
-    } else {
-        return nil, errors.New("no valid idType")
-    }
-    rows, err := db.Query(sqlQuery, id)
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
-    var dmids []uint64
-    for rows.Next() {
-        var dmid uint64
-        err = rows.Scan(&dmid)
-        if err != nil {
-            return nil, err
-        }
-        dmids = append(dmids, dmid)
-    }
-    return dmids, err
+	var sqlQuery string
+	var id uint64
+	if query.ExpenseId > 0 {
+		sqlQuery = "select dmid from DocumentExpenseMapping where eid = $1"
+		id = query.ExpenseId
+	} else if query.DocumentId > 0 {
+		sqlQuery = "select dmid from DocumentExpenseMapping where did = $1"
+		id = query.DocumentId
+	} else {
+		return nil, errors.New("no valid idType")
+	}
+	rows, err := db.Query(sqlQuery, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var dmids []uint64
+	for rows.Next() {
+		var dmid uint64
+		err = rows.Scan(&dmid)
+		if err != nil {
+			return nil, err
+		}
+		dmids = append(dmids, dmid)
+	}
+	return dmids, err
 }
-
