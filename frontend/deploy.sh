f37fc3fd63724467afebf744ef22d1d35931e947
agurk <timothy.moll+github@gmail.com>
2020-02-15 16:30:56 +0100

    Minor refinements

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo docker build . -t expenses
-sudo docker stop expenses
-sudo docker rm expenses
-sudo docker run --name expenses -d -v /data/local/data/resources:/app/resources -p 5000:80 expenses
+readonly NAME="expenses"
+
+docker build . -t $NAME
+docker stop $NAME
+docker rm $NAME expenses
+docker run --name $NAME -d -v /data/local/data/resources:/app/resources -p 5000:80 $NAME
