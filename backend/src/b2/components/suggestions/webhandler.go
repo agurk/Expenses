962d68e3e2ed10032094d45e8a13ed942937a003
agurk <timothy.moll+github@gmail.com>
2020-02-17 15:44:42 +0100

    Feeding the linter

--- backend/src/b2/components/suggestions/webhandler.go
+++ backend/src/b2/components/suggestions/webhandler.go
@@ -8,12 +8,15 @@ import (
 	"net/http"
 )
 
+// WebHandler handles http connections to provide suggestions of changes
+// to an existing expense
 type WebHandler struct {
 	backend  *backend.Backend
 	path     string
 	longpath string
 }
 
+// Instance returns an instantiated webhandler for suggestions
 func Instance(path string, backend *backend.Backend) *WebHandler {
 	handler := new(WebHandler)
 	handler.path = path
@@ -22,14 +25,17 @@ func Instance(path string, backend *backend.Backend) *WebHandler {
 	return handler
 }
 
+// Path returns the path that the webhandler expects to be serving on
 func (handler *WebHandler) Path() string {
 	return handler.path
 }
 
+// LongPath returns the path the webhandler expects to be serving on appended with a trailing /
 func (handler *WebHandler) LongPath() string {
 	return handler.longpath
 }
 
+// Handle is a standard net/http request handler
 func (handler *WebHandler) Handle(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	switch req.Method {
